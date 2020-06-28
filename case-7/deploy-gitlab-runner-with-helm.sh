35230f5a6816c65fdfa92517f67faf6852731753
keremavci <avci.kerem@gmail.com>
2020-06-28 21:02:22 +0000

    Added case-7

---
+++ case-7/deploy-gitlab-runner-with-helm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+kubectl --insecure-skip-tls-verify apply -f create-gitlab-ns.yml
+kubectl --insecure-skip-tls-verify apply -f external-gitlab-svc.yml
+helm repo add gitlab https://charts.gitlab.io
+helm upgrade --install gitlab-runner gitlab/gitlab-runner -f gitlab-runner-helm-values.yml -n gitlab --kubeconfig=/home/kerem/kubeconfig.proxy --version 0.18.0
